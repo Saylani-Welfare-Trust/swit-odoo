6a630009c1969957cc626256dc55381e367cc980
Syed Owais Noor <bytesnode@gmail.com>
2025-11-06 21:11:52 +0500

    Update __manifest__.py

---
+++ custom-addons/bn_pos_order/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'POS Order Customizaiton',
+    'version': '1.0',
+    'author': 'Syed Owais Noor',
+    'website': 'https://bytesnode.com/',
+    'license': 'LGPL-3',
+    'category': 'BytesNode/POS Order Customizaiton',
+    'depends': [
+        'point_of_sale'
+    ],
+    'data': [
+        'reports/duplicate_dn.xml',
+        'views/pos_order.xml',
+    ],
+    'auto_install': False,
+    'application': False,
+}
