2e8d96ebbfd971c20f5f3bcb2c31ae945429d268
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2025-11-19 15:12:23 +0500

    [19 Nov 25]

--- custom-addons/bn_res_company/models/res_company.py
+++ custom-addons/bn_res_company/models/res_company.py
@@ -15,10 +15,10 @@ class ResCompany(models.Model):
     client_secret = fields.Char('Client Secret', tracking=True)
 
     # Welfare
-    welfare_url = fields.Char('Welfare URL', tracking=True)
-    odoo_auth_key = fields.Char('Odoo Auth Key', tracking=True)
+    # welfare_url = fields.Char('Welfare URL', tracking=True)
+    # odoo_auth_key = fields.Char('Odoo Auth Key', tracking=True)
     
-    search_endpoint = fields.Char('Search Endpoint', tracking=True)
-    check_donee_endpoint = fields.Char('Check Donee Endpoint', tracking=True)
-    create_donee_endpoint = fields.Char('Create Donee Endpoint', tracking=True)
-    mark_application_endpoint = fields.Char('Mark Application Endpoint', tracking=True)
+    # search_endpoint = fields.Char('Search Endpoint', tracking=True)
+    # check_donee_endpoint = fields.Char('Check Donee Endpoint', tracking=True)
+    # create_donee_endpoint = fields.Char('Create Donee Endpoint', tracking=True)
+    # mark_application_endpoint = fields.Char('Mark Application Endpoint', tracking=True)
