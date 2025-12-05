c6617b0c9077b4132d90fbcef9e0f3a594a8be28
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2025-12-05 17:42:26 +0500

    [5 Dec 25] [Adding analytic_account_id field in hr public employee]

--- custom-addons/bn_analytic_account_customization/models/__init__.py
+++ custom-addons/bn_analytic_account_customization/models/__init__.py
@@ -2,3 +2,4 @@ from . import analytic_account
 from . import hr_employee
 from . import analytic_distribution_model
 from . import analytical_product_line
+from . import hr_employee_public
---
+++ custom-addons/bn_analytic_account_customization/models/hr_employee_public.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+
+class HREmployeePublic(models.Model):
+    _inherit = 'hr.employee.public'
+
+
+    analytic_account_id = fields.Many2one(related='employee_id.analytic_account_id', compute_sudo=True, string="Analytic Account", store=True)
