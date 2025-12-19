57adf58d08d90bb4d83ac972359ef17173519589
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2025-12-19 20:06:04 +0500

    [19 Dec 25]

--- custom-addons/bn_res_currency/__manifest__.py
+++ custom-addons/bn_res_currency/__manifest__.py
@@ -6,7 +6,7 @@
     'license': 'LGPL-3',
     'category': 'BytesNode/Currency Customization',
     'depends': [
-        'base'
+        'account'
     ],
     'data': [],
     'auto_install': False,
