f896c2031684433649ca412ff318bfc481c3d08f
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2025-12-19 19:56:53 +0500

    Update res_currency.py

--- custom-addons/bn_res_currency/models/res_currency.py
+++ custom-addons/bn_res_currency/models/res_currency.py
@@ -11,12 +11,3 @@ class ResCurrency(models.Model):
         some move lines (either as their foreign currency, or as the main currency).
         """
         return False
-    
-    def write(self, vals):
-        if 'rounding' in vals:
-            rounding_val = vals['rounding']
-            for record in self:
-                if (rounding_val > record.rounding or rounding_val == 0) and record._has_accounting_entries():
-                    raise UserError(_(f"{rounding_val > record.rounding or rounding_val == 0}"))
-
-        return super(ResCurrency, self).write(vals)
