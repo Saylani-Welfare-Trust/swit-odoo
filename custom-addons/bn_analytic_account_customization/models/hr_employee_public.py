ce1446e83eea240fa1153ebd7a2c5dca2a2702a6
Syed Owais Noor <bytesnode@gmail.com>
2025-12-29 15:37:04 +0500

    Update hr_employee_public.py

--- custom-addons/bn_analytic_account_customization/models/hr_employee_public.py
+++ custom-addons/bn_analytic_account_customization/models/hr_employee_public.py
@@ -6,5 +6,5 @@ class HREmployeePublic(models.Model):
 
 
     analytic_account_id = fields.Many2one(related='employee_id.analytic_account_id', compute_sudo=True, string="Analytic Account", store=True)
-    
+
     cnic_no = fields.Char(related='employee_id.cnic_no', compute_sudo=True, string="CNIC No.", store=True)
