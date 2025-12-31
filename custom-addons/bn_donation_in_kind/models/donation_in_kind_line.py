e35f44c292c9a6379307075c80800ee390e64f80
Syed Owais Noor <bytesnode@gmail.com>
2025-12-31 20:00:06 +0500

    Update donation_in_kind_line.py

--- custom-addons/bn_donation_in_kind/models/donation_in_kind_line.py
+++ custom-addons/bn_donation_in_kind/models/donation_in_kind_line.py
@@ -18,6 +18,7 @@ class DonationInKindLine(models.Model):
     product_id = fields.Many2one('product.product', string="Product")
     donation_in_kind_id = fields.Many2one('donation.in.kind', string="Donation In Kind")
     location_id = fields.Many2one('stock.location', string='Location', required=True, domain="[('usage', '=', 'internal')]", default=lambda self: self.default_set_value('location_id'))
+    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
 
     quantity = fields.Float('Quantity')
     avg_price = fields.Float('Average Price', required=True)
