c156ffa1e987c7860783819e1b68aef52f38c932
Syed Owais Noor <bytesnode@gmail.com>
2026-01-13 15:50:45 +0500

    [13 Jan 26] [Adding POS Cheque fields except Deposit Bank]

--- custom-addons/bn_pos_cheque/models/pos_cheque.py
+++ custom-addons/bn_pos_cheque/models/pos_cheque.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 
@@ -15,6 +15,9 @@ class POSCheque(models.Model):
     _description = "POS Cheque"
 
 
+    donor_id = fields.Many2one('res.partner', string="Donor", compute="_set_details", store=True)
+    analytic_account_id = fields.Many2one('account.analytic.account', string="Analytic Acccount", compute="_set_details", store=True)
+    
     name = fields.Char('Name')
     
     state = fields.Selection(selection=state_selection, string="State", default='draft')
@@ -25,6 +28,18 @@ class POSCheque(models.Model):
 
     bounce_count = fields.Integer('Bounce Count')
 
+    amount = fields.Float('Amount', compute="_set_details", store=True)
+
+
+    @api.depends('name')
+    def _set_details(self):
+        for rec in self:
+            pos_order = self.env['pos.order'].search([('pos_cheque_id', '=', rec.id)])
+
+            rec.donor_id = pos_order.partner_id.id
+            rec.analytic_account_id = pos_order.analytic_account_id.id
+            
+            rec.amount = pos_order.amount_total
 
     def action_show_pos_order(self):
         pos_order = self.env['pos.order'].search([('pos_cheque_id', '=', self.id)])
