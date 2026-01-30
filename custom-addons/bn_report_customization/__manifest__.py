58b0dfb25d17deecd250e2c3b2c76461a2049ae8
Syed Owais Noor <bytesnode@gmail.com>
2026-01-30 14:16:49 +0500

    [30 Jan 26] [Adding Bank and Cheque Number in Payment]

---
+++ custom-addons/bn_report_customization/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Report Customization',
+    'version': '1.0',
+    'author': 'Syed Owais Noor',
+    'website': 'http://bytesnode.com',
+    'license': 'LGPL-3',
+    'category': 'BytesNode/Report Customization',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'reports/report_payment_receipt_document.xml',
+    ],
+    'auto_install': False,
+    'application': False,
+}
