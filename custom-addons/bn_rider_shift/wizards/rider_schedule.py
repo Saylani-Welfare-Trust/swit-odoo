2abbd0231abc07a024555f7fee9aa10c19aac008
Syed Owais Noor <bytesnode@gmail.com>
2026-02-06 20:58:27 +0500

    Update rider_schedule.py

--- custom-addons/bn_rider_shift/wizards/rider_schedule.py
+++ custom-addons/bn_rider_shift/wizards/rider_schedule.py
@@ -42,7 +42,7 @@ class RiderSchedule(models.TransientModel):
             lot_ids = key_ids.mapped('lot_id')
             # lot_ids = obj.key_bunch_id.key_ids.filtered(lambda k:k.state == 'issued').mapped('lot_id')
 
-            raise UserError(str(key_ids))
+            # raise UserError(str(key_ids))
 
             # 🔹 Fetch existing collections for these lot_ids
             existing_collections = self.env['rider.collection'].search([
