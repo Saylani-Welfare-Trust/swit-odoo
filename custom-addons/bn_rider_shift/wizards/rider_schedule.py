eb42edc86992a07e4df244f98b910a1011bd1669
Syed Owais Noor <bytesnode@gmail.com>
2026-03-31 19:23:33 +0500

    Update rider_schedule.py

--- custom-addons/bn_rider_shift/wizards/rider_schedule.py
+++ custom-addons/bn_rider_shift/wizards/rider_schedule.py
@@ -100,7 +100,6 @@ class RiderSchedule(models.TransientModel):
                         'donation_box_registration_installation_id': box.id,
                     })
                     
-
                     line_vals.append((0, 0, {
                         'rider_collection_id': collection.id,
                         'day': collection.day,
