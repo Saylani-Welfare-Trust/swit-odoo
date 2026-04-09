843788fc3be7c94fee8a8fd40ae87d78481e64cf
Syed Owais Noor <bytesnode@gmail.com>
2026-04-09 15:03:53 +0500

    Update payment_screen.js

--- custom-addons/bn_qurbani/static/src/screens/payment_screen/payment_screen.js
+++ custom-addons/bn_qurbani/static/src/screens/payment_screen/payment_screen.js
@@ -7,6 +7,8 @@ import { patch } from "@web/core/utils/patch";
 
 patch(PaymentScreen.prototype, {
     async validateOrder(isForceValidate) {
+        const currentOrder = this.currentOrder;
+
         let hasValidProduct = false;
 
         // -------------------------
@@ -47,11 +49,11 @@ patch(PaymentScreen.prototype, {
                 
                 this.env.services.report.doAction("bn_qurbani.qurbani_token_report", [data.id]);
 
-                return;
+                return
             }
         }
 
         // Continue with normal POS flow
-        return super.validateOrder(isForceValidate);
+        super.validateOrder(isForceValidate);
     }
 })
