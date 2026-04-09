55ffc4cda9c2efdb57d7ff802ddbac4a158117a2
Syed Owais Noor <bytesnode@gmail.com>
2026-04-09 14:49:42 +0500

    Update __manifest__.py

--- custom-addons/bn_qurbani/__manifest__.py
+++ custom-addons/bn_qurbani/__manifest__.py
@@ -21,4 +21,9 @@
     ],
     'auto_install': False,
     'application': True,
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'bn_qurbani/static/src/screens/**/*',
+        ],
+    }
 }
