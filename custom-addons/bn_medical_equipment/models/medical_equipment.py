4038bb673064bcee669fc589a80ccf24191f11ed
Abdullah <bytesnode@gmail.com>
2026-04-27 17:45:57 +0500

    Update medical_equipment.py

--- custom-addons/bn_medical_equipment/models/medical_equipment.py
+++ custom-addons/bn_medical_equipment/models/medical_equipment.py
@@ -1041,7 +1041,7 @@ class MedicalEquipment(models.Model):
         for line in self.medical_equipment_line_ids:
             data_list.append({
                 "medicalEquipmentCategoryId": line.medical_equipment_category_id,
-                "productId": line.product_id,
+                "productId": line.product_id.display_name,
                 "quantity": line.quantity,
                 "baseSecurityDeposit": line.base_security_deposit,
                 "actualDepositPercentage": line.actual_deposit_percentage,
