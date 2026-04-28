3a29f659aee095931861679038d910f2d3f4410a
Syed Owais Noor <bytesnode@gmail.com>
2026-04-28 17:01:04 +0500

    Update rider_schedule.py

--- custom-addons/bn_rider_shift/wizards/rider_schedule.py
+++ custom-addons/bn_rider_shift/wizards/rider_schedule.py
@@ -46,7 +46,7 @@ class RiderSchedule(models.TransientModel):
         existing_collections = self.env['rider.collection'].search([
             ('rider_id', '=', employee.id),
             ('lot_id', 'in', lot_ids),
-            # ('state', 'in', active_states),
+            ('state', 'in', active_states),
         ])
 
         existing_lot_ids = existing_collections.mapped('lot_id').ids
