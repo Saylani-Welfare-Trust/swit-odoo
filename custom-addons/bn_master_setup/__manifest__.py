5fc6166a63c84fcea91d96e41f305d3ca4bf8dda
Syed Owais Noor <bytesnode@gmail.com>
2026-04-28 14:10:51 +0500

    Reapply "[28 April 26] [Updating Master Setup]"
    This reverts commit d3543a4a13bcde3319866721ff2ff98e756eceaa.

--- custom-addons/bn_master_setup/__manifest__.py
+++ custom-addons/bn_master_setup/__manifest__.py
@@ -14,8 +14,6 @@
         'data/qurbani_day_data.xml',
         'data/chand_raat_date.xml',
         'data/disbursement_category.xml',
-        'views/location_option.xml',
-        'views/installation_category.xml',
         'views/header_type.xml',
         'views/gateway_config.xml',
         'views/microfinance_scheme.xml',
