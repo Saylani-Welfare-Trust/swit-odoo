663618c8250a37da4b1ed14b13d32df1b443720f
Syed Owais Noor <bytesnode@gmail.com>
2026-04-28 20:58:32 +0500

    [28 April 26]

--- custom-addons/bn_pos_custom_action/models/res_company.py
+++ custom-addons/bn_pos_custom_action/models/res_company.py
@@ -4,10 +4,6 @@ from odoo import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-
-    donation_box_product = fields.Char('Donation Box', tracking=True)
-
-    donation_home_service_product = fields.Char('Donation Home Service', tracking=True)
     
     microfinance_intallement_product = fields.Char('Microfinance Installement', tracking=True)
     microfinance_security_depsoit_product = fields.Char('Microfinance Security Deposit', tracking=True)
