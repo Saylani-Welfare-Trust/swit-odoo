7b3b241e9be6358a9abd308daebbf8927c2d4ab8
Syed Owais Noor <bytesnode@gmail.com>
2026-04-28 21:02:25 +0500

    Update res_company.py

--- custom-addons/bn_pos_custom_action/models/res_company.py
+++ custom-addons/bn_pos_custom_action/models/res_company.py
@@ -5,6 +5,8 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     
+    donation_box_product = fields.Char('Donation Box', tracking=True)
+    
     microfinance_intallement_product = fields.Char('Microfinance Installement', tracking=True)
     microfinance_security_depsoit_product = fields.Char('Microfinance Security Deposit', tracking=True)
     
