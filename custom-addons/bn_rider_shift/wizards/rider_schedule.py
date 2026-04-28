3973ce7c7e460f86e153ece9740ac609bcad25bf
Syed Owais Noor <bytesnode@gmail.com>
2026-04-28 16:37:24 +0500

    Update rider_schedule.py

--- custom-addons/bn_rider_shift/wizards/rider_schedule.py
+++ custom-addons/bn_rider_shift/wizards/rider_schedule.py
@@ -51,6 +51,8 @@ class RiderSchedule(models.TransientModel):
 
         existing_lot_ids = existing_collections.mapped('lot_id').ids
 
+        raise ValidationError(str(existing_lot_ids) + " " + str(lot_ids))
+
         # 🔹 Add existing collections to wizard
         for record in existing_collections:
             line_vals.append((0, 0, {
