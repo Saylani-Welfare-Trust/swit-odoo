8890d769e982d287512ec818df02913a250d55af
Syed Owais Noor <bytesnode@gmail.com>
2026-04-29 18:27:39 +0500

    Update donation_box_registrtaion_installation.py

--- custom-addons/bn_key_management/models/donation_box_registrtaion_installation.py
+++ custom-addons/bn_key_management/models/donation_box_registrtaion_installation.py
@@ -6,9 +6,3 @@ class DonationBoxRegistrationInstallation(models.Model):
 
 
     key_bunch_id = fields.Many2one('key.bunch', string="Key Bunch", tracking=True)
-
-
-    def action_approved(self):
-        self.key_bunch_id.rider_id = self.rider_id.id
-
-        super(DonationBoxRegistrationInstallation, self).action_approved()
