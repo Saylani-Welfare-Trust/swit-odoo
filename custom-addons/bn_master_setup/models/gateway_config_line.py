d2e5215dacc0ce78063cebe4cfbf50be3e6a952e
Syed Owais Noor <bytesnode@gmail.com>
2026-05-01 16:49:51 +0500

    Update gateway_config_line.py

--- custom-addons/bn_master_setup/models/gateway_config_line.py
+++ custom-addons/bn_master_setup/models/gateway_config_line.py
@@ -6,9 +6,9 @@ class GatewayConfigLine(models.Model):
     _description = "Gateway Config Line"
 
 
-    gateway_config_id = fields.Many2one('gateway.config', string="Gateway Config",)
+    gateway_config_id = fields.Many2one('gateway.config', string="Gateway Config")
 
-    product_id = fields.Many2one('product.product', string="Product", ondelete='set null')
+    product_id = fields.Many2one('product.product', string="Product")
     
     name = fields.Char('Product Name')
     
