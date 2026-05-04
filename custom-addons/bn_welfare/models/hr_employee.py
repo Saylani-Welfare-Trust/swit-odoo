850b0aa96b4d8a5dd235f4f5f9408157a88132b4
Abdullah <bytesnode@gmail.com>
2026-05-04 12:11:35 +0500

    update

--- custom-addons/bn_welfare/models/hr_employee.py
+++ custom-addons/bn_welfare/models/hr_employee.py
@@ -3,4 +3,4 @@ from odoo import models, fields, api
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    area = fields.Many2many('bn_master_setup.area', string="Area", tracking=True)
+    area = fields.Many2many('area', string="Area", tracking=True)
