338b79a6c4bd8df49a1cefb5aba9ee0e13894eec
Syed Owais Noor <bytesnode@gmail.com>
2026-05-07 07:40:25 +0500

    Update pos_order.py

--- custom-addons/bn_pos_custom_action/models/pos_order.py
+++ custom-addons/bn_pos_custom_action/models/pos_order.py
@@ -1,5 +1,4 @@
-from odoo import models, fields
-from odoo.exceptions import UserError
+from odoo import models, fields, api
 
 
 class POSOrder(models.Model):
@@ -13,17 +12,25 @@ class POSOrder(models.Model):
     qurbani = fields.Boolean('Qurbani', default=False)
     receive_voucher = fields.Boolean('Receive Voucher', default=False)
 
-    pos_order_seq = fields.Char('POS Order Seq', default=lambda self: self.env['ir.sequence'].next_by_code('pos_order_seqthis'))
+    pos_order_seq = fields.Char('POS Order Seq')
 
 
     def _order_fields(self, ui_order):
         """To get the value of field in pos session to pos order"""
         res = super(POSOrder, self)._order_fields(ui_order)
 
-        res['pos_order_seq'] = ui_order.get('pos_order_seq') or False
         res['pos_cheque_order_id'] = ui_order.get('pos_cheque_order_id') or False
         res['source_document'] = ui_order.get('source_document') or False
         res['receive_voucher'] = ui_order.get('receive_voucher') or False
         res['qurbani'] = ui_order.get('qurbani') or False
 
         return res
+    
+    @api.model
+    def _process_order(self, order, draft, existing_order):
+        pos_order = super()._process_order(order, draft, existing_order)
+
+        if not pos_order.pos_order_seq:
+            pos_order.pos_order_seq = self.env['ir.sequence'].next_by_code('pos_order_seq')
+
+        return pos_order
