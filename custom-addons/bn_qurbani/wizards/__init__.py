26c5cbea3498cc00ff4185468b2e5962ed821a49
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2026-05-11 12:44:40 +0500

    Update __init__.py

--- custom-addons/bn_qurbani/wizards/__init__.py
+++ custom-addons/bn_qurbani/wizards/__init__.py
@@ -2,5 +2,4 @@ from . import qurbani_schedule
 from . import generate_qurbani_demand
 from . import pos_make_payment
 from . import generate_qurbani_slaughter_and_distribution
-from . import generate_qurbani_distribution
 from . import api_order_wizard
