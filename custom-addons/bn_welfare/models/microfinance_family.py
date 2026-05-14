49eb7794ab832b06149f94ac47733fbe9ba2093d
Abdulrafay5712 <111308220+Abdulrafay5712@users.noreply.github.com>
2026-05-14 19:37:18 +0500

    Update microfinance_family.py

--- custom-addons/bn_welfare/models/microfinance_family.py
+++ custom-addons/bn_welfare/models/microfinance_family.py
@@ -54,17 +54,49 @@ class MicrofinanceFamily(models.Model):
                 }
             }
         
-        # Open disbursement lines in a list view with all available fields
+        # Create a dynamic tree view with all disbursement line columns
+        tree_arch = """
+        <tree>
+            <field name="disbursement_category_id" options="{'no_create': True, 'no_edit': True, 'no_open': True}"/>
+            <field name="disbursement_application_type_id" options="{'no_create': True, 'no_edit': True, 'no_open': True}"/>
+            <field name="product_id" options="{'no_create': True, 'no_edit': True, 'no_open': True}"/>
+            <field name="payment_type" />
+            <field name="assigned_officer_id" options="{'no_create': True, 'no_edit': True, 'no_open': True}"/>
+            <field name="collection_point" />
+            <field name="analytic_account_id" options="{'no_create': True, 'no_edit': True, 'no_open': True}"/>
+            <field name="collection_date" />
+            <field name="marriage_date"/>
+            <field name="quantity" />
+            <field name="amount" readonly="1" />
+            <field name="total_amount" readonly="1" />
+            <field name="state" widget="badge" decoration-warning="state == 'draft'" decoration-success="state == 'disbursed'" />
+        </tree>
+        """
+        
+        # Get or create the view
+        view_obj = self.env['ir.ui.view']
+        view_name = f'welfare_line_history_{self.id}_{self.cnic_no.replace("-", "_")}'
+        
+        existing_view = view_obj.search([('name', '=', view_name)], limit=1)
+        if existing_view:
+            tree_view = existing_view
+        else:
+            tree_view = view_obj.create({
+                'name': view_name,
+                'model': 'welfare.line',
+                'arch': tree_arch,
+                'type': 'tree',
+            })
+        
+        # Open disbursement lines using the dynamic tree view
         return {
             'type': 'ir.actions.act_window',
             'name': f'Disbursement History - CNIC: {self.cnic_no}',
             'res_model': 'welfare.line',
-            'view_mode': 'tree,form',
-            'view_id': self.env.ref('bn_welfare.view_welfare_line_tree').id if self.env.ref('bn_welfare.view_welfare_line_tree', raise_if_not_found=False) else False,
+            'view_type': 'tree',
+            'view_id': tree_view.id,
+            'views': [(tree_view.id, 'tree')],
             'domain': [('id', 'in', disbursement_lines.ids)],
-            'context': {
-                'create': False,  # Disable creating new lines from history view
-                'edit': False,    # Disable editing from history view (optional)
-            },
+            'context': {'create': False},
             'target': 'current',
         }
