221d8d02bbf8324466244c030114de5f296121c8
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2026-05-15 15:05:04 +0500

    Update receiving_popup.js

--- custom-addons/bn_pos_custom_action/static/src/app/receiving_popup/receiving_popup.js
+++ custom-addons/bn_pos_custom_action/static/src/app/receiving_popup/receiving_popup.js
@@ -400,6 +400,8 @@ export class ReceivingPopup extends AbstractAwaitablePopup {
     async processQurbaniRecord(selectedOrder) {
         try {
 
+            console.log(this);
+
             let found = false;
 
             // ============================================
