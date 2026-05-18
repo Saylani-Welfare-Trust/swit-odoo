e0a57375a1f13df89d6a87f4d2099100f041722d
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2026-05-18 20:25:25 +0500

    Update pos_session.py

--- custom-addons/bn_closing_session/models/pos_session.py
+++ custom-addons/bn_closing_session/models/pos_session.py
@@ -185,7 +185,7 @@ class PosSession(models.Model):
                     'partner_id': False,
                 })
 
-    def _create_bank_payment_moves(self, data):
+    def _create_combine_account_payment(self, payment_method, amounts, diff_amount):
         # -------------------------------------------------
         # SKIP STANDARD COMBINED BANK PAYMENT MOVES
         # -------------------------------------------------
@@ -194,30 +194,32 @@ class PosSession(models.Model):
                 "Skipping _create_bank_payment_moves "
                 "for split accounting"
             )
-            return data
+            return
+        
+        outstanding_account = payment_method.outstanding_account_id or self.company_id.account_journal_payment_debit_account_id
+        destination_account = self._get_receivable_account(payment_method)
 
-        combine_receivables_bank = data.get('combine_receivables_bank')
-        split_receivables_bank = data.get('split_receivables_bank')
-        bank_payment_method_diffs = data.get('bank_payment_method_diffs')
-        MoveLine = data.get('MoveLine')
-        payment_method_to_receivable_lines = {}
-        payment_to_receivable_lines = {}
-        for payment_method, amounts in combine_receivables_bank.items():
-            combine_receivable_line = MoveLine.create(self._get_combine_receivable_vals(payment_method, amounts['amount'], amounts['amount_converted']))
-            payment_receivable_line = self._create_combine_account_payment(payment_method, amounts, diff_amount=bank_payment_method_diffs.get(payment_method.id) or 0)
-            payment_method_to_receivable_lines[payment_method] = combine_receivable_line | payment_receivable_line
+        if float_compare(amounts['amount'], 0, precision_rounding=self.currency_id.rounding) < 0:
+            # revert the accounts because account.payment doesn't accept negative amount.
+            outstanding_account, destination_account = destination_account, outstanding_account
 
-        for payment, amounts in split_receivables_bank.items():
-            split_receivable_line = MoveLine.create(self._get_split_receivable_vals(payment, amounts['amount'], amounts['amount_converted']))
-            payment_receivable_line = self._create_split_account_payment(payment, amounts)
-            payment_to_receivable_lines[payment] = split_receivable_line | payment_receivable_line
+        account_payment = self.env['account.payment'].create({
+            'amount': abs(amounts['amount']),
+            'journal_id': payment_method.journal_id.id,
+            'force_outstanding_account_id': outstanding_account.id,
+            'destination_account_id':  destination_account.id,
+            'ref': _('Combine %s POS payments from %s', payment_method.name, self.name),
+            'pos_payment_method_id': payment_method.id,
+            'pos_session_id': self.id,
+            'company_id': self.company_id.id,
+        })
 
-        for bank_payment_method in self.payment_method_ids.filtered(lambda pm: pm.type == 'bank' and pm.split_transactions):
-            self._create_diff_account_move_for_split_payment_method(bank_payment_method, bank_payment_method_diffs.get(bank_payment_method.id) or 0)
+        diff_amount_compare_to_zero = self.currency_id.compare_amounts(diff_amount, 0)
+        if diff_amount_compare_to_zero != 0:
+            self._apply_diff_on_account_payment_move(account_payment, payment_method, diff_amount)
 
-        data['payment_method_to_receivable_lines'] = payment_method_to_receivable_lines
-        data['payment_to_receivable_lines'] = payment_to_receivable_lines
-        return data
+        account_payment.action_post()
+        return account_payment.move_id.line_ids.filtered(lambda line: line.account_id == account_payment.destination_account_id)
 
     # ------------------------------------------------------------
     # MAIN SPLIT LOGIC (replaces standard receivable lines)
