ec132784676c2833fafd81b1057a3433f5e683ed
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2026-05-19 21:46:54 +0500

    Update direct_depsoit.py

--- custom-addons/bn_qurbani/models/direct_depsoit.py
+++ custom-addons/bn_qurbani/models/direct_depsoit.py
@@ -83,7 +83,7 @@ class DirectDeposit(models.Model):
 
         qurbani_details = self.env['qurbani.order'].create_qurbani_record(data)
 
-        raise UserError(qurbani_details)
+        raise UserError(qurbani_details.get('id'))
 
         self.qurbani_order_id = qurbani_details.get('id')
 
