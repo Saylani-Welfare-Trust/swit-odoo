72d16243d471d7e9511e1a78dfb69e913740f275
Abdullah <bytesnode@gmail.com>
2026-05-22 11:31:47 +0500

    update

--- custom-addons/bn_res_qurbani/models/res_company.py
+++ custom-addons/bn_res_qurbani/models/res_company.py
@@ -5,4 +5,4 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
 
-    web_no_meat_distribution_location = fields.Char('Web No-Meat Distribution Location', tracking=True)
+    web_no_meat_distribution_location_id = fields.Many2one('stock.location', string="Web No-Meat Distribution Location", tracking=True)
