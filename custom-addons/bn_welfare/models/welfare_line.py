b672101cbce5eb0d459553eea85c02d2a99c039a
Abdulrafay5712 <bytesnode@gmail.com>
2026-05-22 13:18:57 +0500

    Update welfare_line.py

--- custom-addons/bn_welfare/models/welfare_line.py
+++ custom-addons/bn_welfare/models/welfare_line.py
@@ -61,8 +61,8 @@ class WelfareLine(models.Model):
         default=lambda self: self.env.ref('bn_welfare.assigned_officer_hr_employee_category', raise_if_not_found=False).id if self.env.ref('bn_welfare.assigned_officer_hr_employee_category', raise_if_not_found=False) else False
     )
     # order_type field moved to main welfare model
-    collection_point = fields.Selection(selection=collection_point_selection, string="Collection Point", store=True )
-    payment_type = fields.Selection(selection=payment_type_selection, string="Payment Type", default='self', store=True)
+    collection_point = fields.Selection(selection=collection_point_selection, string="Collection Point" )
+    payment_type = fields.Selection(selection=payment_type_selection, string="Payment Type", default='self')
     assigned_officer_id = fields.Many2one('hr.employee', string="Assigned Officer (Marfat)", domain="[('category_ids', 'in', [employee_category_id_officer])]")
     recurring_duration = fields.Selection(selection=recurring_duration_selection, string="Recurring Duration")
     state = fields.Selection(selection=state_selection, string="State", default='draft')
