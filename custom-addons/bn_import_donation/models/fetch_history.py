37e5006fdcd2179c4b92fdbde5f451c267fa997c
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2026-05-25 20:37:56 +0500

    [25 May 26]

--- custom-addons/bn_import_donation/models/fetch_history.py
+++ custom-addons/bn_import_donation/models/fetch_history.py
@@ -15,6 +15,11 @@ class FetchHistory(models.Model):
     journal_entry_id = fields.Many2one('account.move', string="Journal Entry")
     picking_id = fields.Many2one('stock.picking', string="Picking")
 
+    state = fields.Selection([
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ], default='in_progress')
+
 
     def show_stock_picking(self):
         return {
