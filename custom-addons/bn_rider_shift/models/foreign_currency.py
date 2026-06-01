b423e816ad416ddb1bc777cc085fbe952a200595
Abdulrafay5712 <bytesnode@gmail.com>
2026-06-01 17:21:02 +0500

    update

--- custom-addons/bn_rider_shift/models/foreign_currency.py
+++ custom-addons/bn_rider_shift/models/foreign_currency.py
@@ -37,10 +37,11 @@ class ForeignCurrency(models.Model):
     def action_reject(self):
         self.state = 'reject'
 
-    def action_create_pos_record(self):
-        """Create a POS order for the selected foreign currency lines."""
-        if 'pos.order' not in self.env:
-            raise UserError('POS module is not available in this database.')
+    def action_create_donation_box_request(self):
+        """Create a Donation Box request for the selected foreign currency lines."""
+        donation_box_model = self.env['ir.model'].search([('model', '=', 'donation.box.request')], limit=1)
+        if not donation_box_model:
+            raise UserError('Donation Box module is not available in this database.')
 
         invalid_lines = self.filtered(lambda rec: rec.state != 'converted')
         if invalid_lines:
@@ -50,45 +51,22 @@ class ForeignCurrency(models.Model):
         if not selected_amount:
             raise UserError('Please select converted foreign currency lines with a non-zero exchanged amount.')
 
-        session = self.env['pos.session'].search([('state', '=', 'opened')], limit=1)
-        if not session:
-            session = self.env['pos.session'].search([], limit=1)
-        if not session:
-            raise UserError('No POS session available to create a POS order.')
-
-        product = self.env['product.product'].search([('sale_ok', '=', True)], limit=1)
-        if not product:
-            product = self.env['product.product'].search([], limit=1)
-        if not product:
-            raise UserError('No product found to create POS order lines.')
-
         current_time = fields.Datetime.context_timestamp(self, fields.Datetime.now())
-        pos_name = f"FCB-{current_time.strftime('%Y%m%d%H%M%S')}"
-        order_vals = {
-            'name': pos_name,
-            'session_id': session.id,
-            'user_id': session.user_id.id or self.env.uid,
-            'amount_total': selected_amount,
-            'amount_tax': 0.0,
-            'amount_paid': selected_amount,
-            'amount_return': 0.0,
-            'state': 'paid',
-            'lines': [(0, 0, {
-                'product_id': product.id,
-                'name': f'Foreign currency total for {len(self)} line(s)',
-                'qty': 1,
-                'price_unit': selected_amount,
-                'price_subtotal': selected_amount,
-                'price_subtotal_incl': selected_amount,
-            })],
-        }
-        pos_order = self.env['pos.order'].create(order_vals)
+        donation_box_name = f"FCB-{current_time.strftime('%Y%m%d%H%M%S')}"
+
+        donation_box_request = self.env['donation.box.request'].create({
+            'name': donation_box_name,
+        })
+
+        donation_box_request.message_post(
+            body=f'Total selected foreign currency amount: {selected_amount}',
+        )
 
         return {
-            'name': 'POS Order',
+            'name': 'Donation Box Request',
             'type': 'ir.actions.act_window',
-            'res_model': 'pos.order',
-            'res_id': pos_order.id,
+            'res_model': 'donation.box.request',
+            'res_id': donation_box_request.id,
             'view_mode': 'form',
             'target': 'current',
         }
