b183d67b828a0b361ae524dc1a43aaf8232977e7
Abdulrafay5712 <bytesnode@gmail.com>
2026-06-07 00:10:39 +0500

    update product domain in welfare limit of master setup

--- custom-addons/bn_welfare/models/welfare_limit.py
+++ custom-addons/bn_welfare/models/welfare_limit.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
+
 class WelfareApprovalLimit(models.Model):
     _name = 'welfare.approval.limit'
     _description = "Welfare Approval Limits"
@@ -8,15 +9,39 @@ class WelfareApprovalLimit(models.Model):
 
     group_id = fields.Many2one('res.groups', string="User Group", required=True)
     group_name = fields.Char(related='group_id.name', string="Group Name", store=True)
-    
+
     # Limit settings
     max_amount_limit = fields.Monetary(string="Maximum Approval Amount", required=True)
-    currency_id = fields.Many2one('res.currency', 'Currency', 
-                                  default=lambda self: self.env.company.currency_id)
-    
-    # Product limits using your product.master
-    allowed_product_master_ids = fields.Many2many('product.master', string="Allowed Products")
-    
+    currency_id = fields.Many2one(
+        'res.currency', 
+        'Currency',
+        default=lambda self: self.env.company.currency_id
+    )
+    disbursement_application_type_id = fields.Many2one(
+        'disbursement.application.type', 
+        string="Disbursement Application Type"
+    )
 
-    
+    # Product domain filter
+    product_domain = fields.Char(
+        string="Product Domain",
+        compute='_compute_product_domain',
+        store=True,
+        default="[]"
+    )
+
     active = fields.Boolean(string="Active", default=True)
+
+    @api.depends('disbursement_application_type_id.product_category_id')
+    def _compute_product_domain(self):
+        for rec in self:
+            product_category = rec.disbursement_application_type_id.product_category_id
+            if not product_category:
+                rec.product_domain = "[]"
+                continue
+
+            products = self.env['product.product'].search([
+                ('categ_id', 'child_of', product_category.id),
+                ('product_tmpl_id.is_welfare', '=', True),
+            ])
+            rec.product_domain = str([('id', 'in', products.ids)])
