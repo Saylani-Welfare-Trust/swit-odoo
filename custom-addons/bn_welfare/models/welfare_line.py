a032ee0a72d23674890842daca4ee32e377a3bd4
Abdulrafay5712 <bytesnode@gmail.com>
2026-06-06 21:53:22 +0500

    Update welfare_line.py

--- custom-addons/bn_welfare/models/welfare_line.py
+++ custom-addons/bn_welfare/models/welfare_line.py
@@ -447,21 +447,16 @@ class WelfareLine(models.Model):
     @api.depends('disbursement_application_type_id.product_category_id')
     def _compute_product_domain(self):
         for rec in self:
-            # product_category = rec.disbursement_application_type_id.product_category_id
-            # if not product_category:
-            #     rec.product_domain = "[]"
-            #     continue
-
-            # products = self.env['product.product'].search([
-            #     ('categ_id', 'child_of', product_category.id),
-            #     ('is_welfare', '=', True),
-            # ])
-            # rec.product_domain = str([('id', 'in', products.ids)])
+            product_category = rec.disbursement_application_type_id.product_category_id
+            if not product_category:
+                rec.product_domain = [(5, 0, 0)]
+                continue
 
             products = self.env['product.product'].search([
-                ('product_template_id.is_welfare', '=', True),
+                ('categ_id', 'child_of', product_category.id),
+                ('is_welfare', '=', True),
             ])
-            rec.product_domain = str([('id', 'in', products.ids)])
+            rec.product_domain = [(6, 0, products.ids)]
     @api.depends('disbursement_application_type_id')
     def _compute_analytic_account_domain(self):
         for rec in self:
