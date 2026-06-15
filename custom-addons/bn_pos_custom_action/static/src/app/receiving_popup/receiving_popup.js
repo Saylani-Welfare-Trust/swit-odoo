70f4f7b728514d71e2932ba376b4707eb0040ff3
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2026-06-15 13:02:41 +0500

    Update receiving_popup.js

--- custom-addons/bn_pos_custom_action/static/src/app/receiving_popup/receiving_popup.js
+++ custom-addons/bn_pos_custom_action/static/src/app/receiving_popup/receiving_popup.js
@@ -1099,11 +1099,10 @@ export class ReceivingPopup extends AbstractAwaitablePopup {
                         quantity: quantity || 1,
                     });
                 }
-                else {
+                else if (product.lst_price <= 0) {
                     selectedOrder.add_product(product, {
                         quantity: quantity || 1,
-                        price_extra: line.amount,
-                        // price_extra: line.security_deposit || product.lst_price || line.amount,
+                        price_extra: line.security_deposit || product.lst_price,
                     });
                 }
             }
@@ -1117,7 +1116,7 @@ export class ReceivingPopup extends AbstractAwaitablePopup {
             else if (product.lst_price <= 0) {
                 selectedOrder.add_product(product, {
                     quantity: quantity || 1,
-                    price_extra: line.security_deposit || product.lst_price,
+                    price_extra: line.security_deposit || product.lst_price || line.amount,
                 });
             }
 
