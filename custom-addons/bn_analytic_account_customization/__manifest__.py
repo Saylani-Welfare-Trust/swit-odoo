8c5c707d60a396c1b32ac6ea0c396ebe27d5726e
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2026-06-17 16:55:40 +0500

    Update __manifest__.py

--- custom-addons/bn_analytic_account_customization/__manifest__.py
+++ custom-addons/bn_analytic_account_customization/__manifest__.py
@@ -12,12 +12,12 @@
     ],
     'data': [
         'security/ir.model.access.csv',
-        
+
         'views/analytic_account.xml',
         'views/analytic_distribution_model.xml',
         'views/hr_employee.xml',
-        'views/sub_zone.xml',
         'views/location_option.xml',
+        'views/sub_zone.xml',
     ],
     'auto_install': False,
     'application': False,
