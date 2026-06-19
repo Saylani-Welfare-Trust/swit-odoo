0a029450261140857dd187024abb942c814268ab
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2026-06-19 09:16:22 +0500

    Revert "[19 June 26] [Donation In Kind and Donation Box]"
    This reverts commit f41b6e9ba037b72576c3f9c60a1c2bacd070cafd.

--- custom-addons/bn_donation_in_kind/__manifest__.py
+++ custom-addons/bn_donation_in_kind/__manifest__.py
@@ -10,7 +10,6 @@
     ],
     'data': [
         'data/sequence.xml',
-        'data/server_action.xml',
         'security/group.xml',
         'security/ir.model.access.csv',
         'views/donation_in_kind.xml',
