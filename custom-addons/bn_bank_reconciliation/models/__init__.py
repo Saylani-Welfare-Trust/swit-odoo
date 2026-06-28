16111a999b4cd95f080fde64026e6167bc91d47e
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2026-06-28 16:26:01 +0500

    [28 June 26] [Removing unnecessary modules]

--- custom-addons/bn_bank_reconciliation/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import bank_reconciliation_master
-from . import bank_reconciliation_transaction
-from . import bank_reconciliation_reconciled
-from . import account_move
-from . import account_move_line
