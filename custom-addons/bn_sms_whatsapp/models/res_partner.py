074e8fbe30f191c5fe109a8ca63f103bc38ea754
Abdulrafay5712 <bytesnode@gmail.com>
2026-06-29 20:16:38 +0500

    Update res_partner.py

--- custom-addons/bn_sms_whatsapp/models/res_partner.py
+++ custom-addons/bn_sms_whatsapp/models/res_partner.py
@@ -1,9 +1,14 @@
-
 from odoo import models, fields, api
-from odoo.exceptions import UserError  # <-- Ye import karna hai
+from odoo.exceptions import UserError  
 import logging
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-
     whatsapp = fields.Char(string="WhatsApp")
+
+    @api.onchange('mobile')
+    def _onchange_mobile(self):
+        """Auto copy mobile number to whatsapp field"""
+        if self.mobile:
+            self.whatsapp = self.mobile
