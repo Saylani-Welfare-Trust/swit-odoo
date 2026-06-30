f0948fbb96b58284002817736cb59cc5afa20fd3
Abdulrafay5712 <bytesnode@gmail.com>
2026-06-30 13:25:23 +0500

    Revert "update"
    This reverts commit d910a7b6a36e3ddbcebbab7f7528740a8fa883e2.

--- custom-addons/bn_qurbani/models/hijri.py
+++ custom-addons/bn_qurbani/models/hijri.py
@@ -1,5 +1,4 @@
-from odoo import models, fields, api
-from odoo.exceptions import UserError
+from odoo import models, fields
 
 
 class Hijri(models.Model):
@@ -7,33 +6,5 @@ class Hijri(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = 'Hijri'
 
+
     name = fields.Char('Hijri', tracking=True)
-    state = fields.Selection(
-        [
-            ('draft', 'Draft'),
-            ('approved', 'Approved'),
-        ],
-        string='Status',
-        default='draft',
-        tracking=True,
-        copy=False,
-    )
-
-    def action_approve(self):
-        for rec in self:
-            if rec.state == 'approved':
-                continue
-            rec.state = 'approved'
-
-    def action_reset_to_draft(self):
-        for rec in self:
-            rec.state = 'draft'
-
-    def write(self, vals):
-        # Block edits on approved records unless the user is an approver
-        # (or the only change being made is the state itself).
-        if set(vals.keys()) - {'state'}:
-            for rec in self:
-                if rec.state == 'approved' and not self.env.user.has_group('bn_qurbani.group_hijri_approver'):
-                    raise UserError("This record is approved and locked. Only an approver can edit it.")
-        return super().write(vals)
