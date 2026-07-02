603257603a71619a019ba32fdbeb4695b8a0a2e8
Abdulrafay5712 <bytesnode@gmail.com>
2026-07-02 20:15:53 +0500

    Update base_module.py

--- custom-addons/bn_microfinance/models/base_module.py
+++ custom-addons/bn_microfinance/models/base_module.py
@@ -3,6 +3,7 @@ from odoo.exceptions import UserError, ValidationError
 
 class BaseModule(models.Model):
     _inherit = 'base'
+    _name = 'base'
 
     def unlink(self):
         raise UserError(_('You cannot delete a record.'))
