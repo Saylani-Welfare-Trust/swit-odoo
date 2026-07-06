69463241a1df3298a68aaeb3bb020dcf098d8605
Syed Owais Noor <200616713+BytesNode@users.noreply.github.com>
2026-07-06 11:23:25 +0500

    [6 July 26] [Updating whatsapp number]

--- custom-addons/bn_sms_whatsapp/models/res_partner.py
+++ custom-addons/bn_sms_whatsapp/models/res_partner.py
@@ -8,25 +8,15 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+
     whatsapp = fields.Char(string="WhatsApp")
 
-    @api.onchange('mobile')
-    def _onchange_mobile(self):
-        """Auto copy mobile number to whatsapp field (UI form use)"""
-        if self.mobile:
-            self.whatsapp = self.mobile
 
     @api.model_create_multi
-    def create(self, vals_list):
+    def create(self, partner):
         """Auto copy mobile to whatsapp when partner is created
         from anywhere, including POS"""
-        for vals in vals_list:
-            if vals.get('mobile') and not vals.get('whatsapp'):
-                vals['whatsapp'] = vals['mobile']
-        return super().create(vals_list)
-
-    def write(self, vals):
-        """Keep whatsapp in sync if mobile is updated later"""
-        if vals.get('mobile') and not vals.get('whatsapp'):
-            vals['whatsapp'] = vals['mobile']
-        return super().write(vals)
+        if partner.get('mobile'):
+            partner['whatsapp'] = partner['mobile']
+        
+        return super().create(partner)
