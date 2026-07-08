518c203151c363c9162a0860651971af3b32b15a
Abdulrafay5712 <bytesnode@gmail.com>
2026-07-08 16:05:12 +0500

    update

--- custom-addons/bn_qurbani/models/hijri.py
+++ custom-addons/bn_qurbani/models/hijri.py
@@ -15,15 +15,15 @@ class Hijri(models.Model):
     def _compute_can_create_new(self):
         """Compute if user can create new records"""
         for record in self:
-            unapproved_count = self.search_count([('approved', '=', False)])
+            unapproved_count = self.search_count([('approved', '=', True)])
             record.can_create_new = unapproved_count == 0
     
     @api.model_create_multi
     def create(self, vals_list):
         """Override create to enforce rule"""
-        unapproved_count = self.search_count([('approved', '=', False)])
+        unapproved_count = self.search_count([('approved', '=', True)])
         if unapproved_count > 0:
             raise exceptions.UserError(
-                "Cannot create new records. Please approve all existing unapproved records first."
+                "Cannot create new records. Please unchecked approved records first."
             )
         return super(Hijri, self).create(vals_list)
