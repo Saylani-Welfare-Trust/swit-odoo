1742fc191ddc2e80c1474b656dc0fc29cc8d84f7
Abdulrafay5712 <bytesnode@gmail.com>
2026-07-23 19:20:39 +0500

    update

--- custom-addons/bn_access_right/models/chatter.py
+++ custom-addons/bn_access_right/models/chatter.py
@@ -4,38 +4,88 @@ class ResGroups(models.Model):
     _inherit = 'res.groups'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     
-    # Simple computed fields to show user info
+    # Computed fields for user count and names
     user_count = fields.Integer(
-        string="User Count",
+        string="Count",
         compute='_compute_user_info',
-        store=False,
+        store=True,  # Stored for performance
+        tracking=True  # Track changes
     )
     
     user_names = fields.Char(
-        string="User Names",
+        string="Names",
         compute='_compute_user_info',
-        store=False,
+        store=True,  # Stored for performance
+        tracking=True  # Track changes
     )
     
-    @api.depends('res_users.groups_id')
+    @api.depends('res_users.groups_id')  # Correct dependency - watches the inverse relation
     def _compute_user_info(self):
         for group in self:
+            # Get users through the inverse relation
             users = self.env['res.users'].search([('groups_id', 'in', group.id)])
             group.user_count = len(users)
             group.user_names = ', '.join(users.mapped('display_name')) if users else 'No users'
+    
+    def write(self, vals):
+        # Track user changes when groups are modified
+        if 'users' in vals or 'user_ids' in vals:
+            # Get current users before the change
+            old_users = self.env['res.users'].search([('groups_id', 'in', self.ids)])
+            
+        result = super(ResGroups, self).write(vals)
+        
+        # Post chatter messages for user changes
+        if 'users' in vals or 'user_ids' in vals:
+            for group in self:
+                # Get new users
+                new_users = self.env['res.users'].search([('groups_id', 'in', group.id)])
+                
+                # Find added and removed users
+                added_users = new_users - old_users
+                removed_users = old_users - new_users
+                
+                # Get current user who made the change
+                current_user = self.env.user
+                
+                # Post message for added users
+                if added_users:
+                    user_names = ', '.join(added_users.mapped('display_name'))
+                    group.message_post(
+                        body=f"<b>Users Added:</b> {user_names}<br/>"
+                             f"<b>Added by:</b> {current_user.display_name}<br/>"
+                             f"<b>Total Users:</b> {group.user_count}<br/>"
+                             f"<b>All Users:</b> {group.user_names}",
+                        subject="Users Added to Group",
+                        subtype_xmlid='mail.mt_note'
+                    )
+                
+                # Post message for removed users
+                if removed_users:
+                    user_names = ', '.join(removed_users.mapped('display_name'))
+                    group.message_post(
+                        body=f"<b>Users Removed:</b> {user_names}<br/>"
+                             f"<b>Removed by:</b> {current_user.display_name}<br/>"
+                             f"<b>Total Users:</b> {group.user_count}<br/>"
+                             f"<b>All Users:</b> {group.user_names}",
+                        subject="Users Removed from Group",
+                        subtype_xmlid='mail.mt_note'
+                    )
+        
+        return result
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
     
     def write(self, vals):
-        # Store old groups before changes
+        # Track if groups are being modified from the user side
         if 'groups_id' in vals:
+            # Store old groups before the change
             old_groups = {user.id: set(user.groups_id.ids) for user in self}
         
         result = super(ResUsers, self).write(vals)
         
-        # Post messages for group changes
         if 'groups_id' in vals:
             current_user = self.env.user
             for user in self:
@@ -47,9 +97,13 @@ class ResUsers(models.Model):
                 if added_groups:
                     groups = self.env['res.groups'].browse(list(added_groups))
                     for group in groups:
+                        # Recompute user info
+                        group._compute_user_info()
                         group.message_post(
                             body=f"<b>User Added:</b> {user.display_name}<br/>"
-                                 f"<b>Added by:</b> {current_user.display_name}",
+                                 f"<b>Added by:</b> {current_user.display_name}<br/>"
+                                 f"<b>Total Users:</b> {group.user_count}<br/>"
+                                 f"<b>All Users:</b> {group.user_names}",
                             subject="User Added to Group",
                             subtype_xmlid='mail.mt_note'
                         )
@@ -59,9 +113,13 @@ class ResUsers(models.Model):
                 if removed_groups:
                     groups = self.env['res.groups'].browse(list(removed_groups))
                     for group in groups:
+                        # Recompute user info
+                        group._compute_user_info()
                         group.message_post(
                             body=f"<b>User Removed:</b> {user.display_name}<br/>"
-                                 f"<b>Removed by:</b> {current_user.display_name}",
+                                 f"<b>Removed by:</b> {current_user.display_name}<br/>"
+                                 f"<b>Total Users:</b> {group.user_count}<br/>"
+                                 f"<b>All Users:</b> {group.user_names}",
                             subject="User Removed from Group",
                             subtype_xmlid='mail.mt_note'
                         )
