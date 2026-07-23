a470d3a8f71a0a9b233814b682cfaffda74bd4eb
Abdulrafay5712 <bytesnode@gmail.com>
2026-07-23 18:49:18 +0500

    Update chatter.py

--- custom-addons/bn_access_right/models/chatter.py
+++ custom-addons/bn_access_right/models/chatter.py
@@ -3,9 +3,15 @@ from odoo import api, fields, models
 class ResGroups(models.Model):
     _inherit = 'res.groups'
     _inherit = ['mail.thread', 'mail.activity.mixin'] 
-
-
     
+    # Add the users field explicitly if it doesn't exist
+    users = fields.Many2many(
+        'res.users',
+        'res_users_groups_rel',
+        'gid',
+        'uid',
+        string='Users'
+    )
     
     user_count = fields.Integer(
         string="User Count",
@@ -27,3 +33,15 @@ class ResGroups(models.Model):
             users = group.users
             group.user_count = len(users)
             group.user_names = ', '.join(users.mapped('display_name')) if users else 'No users'
+    
+    # Override write to add chatter messages
+    def write(self, vals):
+        result = super(ResGroups, self).write(vals)
+        # If users were modified, post to chatter
+        if 'users' in vals:
+            for record in self:
+                record.message_post(
+                    body=f"<b>Users updated</b><br/>Current users: {record.user_names}",
+                    subject="User Update Notification"
+                )
+        return result
