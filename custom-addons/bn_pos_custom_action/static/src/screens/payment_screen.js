4c6cfcd86b36c197d39dbee8d0b2ea6e70365d07
BytesNode Developer <bytesnode@gmail.com>
2026-08-10 15:09:07 +0500

    Update payment_screen.js

--- custom-addons/bn_pos_custom_action/static/src/screens/payment_screen.js
+++ custom-addons/bn_pos_custom_action/static/src/screens/payment_screen.js
@@ -11,6 +11,7 @@ patch(PaymentScreen.prototype, {
 
         console.log(this);
         
+        
         // Only process medical equipment if order has extra_data with medical_equipment
         if (currentOrder && currentOrder.extra_data && currentOrder.extra_data.medical_equipment) {
             const medicalData = currentOrder.extra_data.medical_equipment;
