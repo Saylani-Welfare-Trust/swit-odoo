8237b691e3903804c9e9adf334ba9435e96de1fb
Abdulrafay5712 <bytesnode@gmail.com>
2026-08-11 18:05:19 +0500

    update

--- custom-addons/bn_analytic_account_customization/models/analytic_account.py
+++ custom-addons/bn_analytic_account_customization/models/analytic_account.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 class AnalyticAccount(models.Model):
     _inherit = 'account.analytic.account'
 
-
     location_option_id = fields.Many2one('location.option', string="Location Option", tracking=True)
-
     address = fields.Text('Address', tracking=True)
+    parent_id = fields.Many2one('account.analytic.account', string="Parent City", tracking=True,
+                                 domain="[('location_option_id.name', '=', 'City')]")
