c8b6993fcaa4dc386aed967724d064dd212ba38d
Abdulrafay5712 <bytesnode@gmail.com>
2026-08-18 11:35:26 +0500

    Revert "[18 Aug 26] [Applying Sequence code again]"
    This reverts commit b5ff33ce456a305cf7d152791c329d5ff8793196.

--- custom-addons/bn_pos_custom_action/static/src/screens/payment_screen.js
+++ custom-addons/bn_pos_custom_action/static/src/screens/payment_screen.js
@@ -6,73 +6,10 @@ import { _t } from "@web/core/l10n/translation";
 import { patch } from "@web/core/utils/patch";
 
 patch(PaymentScreen.prototype, {
-    _getCounterKey(config_id) {
-        return `counter_${config_id}`;
-    },
-
-    _getCounter() {
-        const config_id = this.pos.config.id;
-        const key = this._getCounterKey(config_id);
-
-        const currentYear = new Date().getFullYear();
-
-        let counterData = JSON.parse(
-            localStorage.getItem(key) || "null"
-        );
-
-        // First POS opening
-        if (!counterData) {
-            counterData = {
-                year: currentYear,
-                counter: this.pos.config.sequence || 1,
-            };
-
-            localStorage.setItem(
-                key,
-                JSON.stringify(counterData)
-            );
-        }
-
-        // New year reset
-        if (counterData.year !== currentYear) {
-            counterData = {
-                year: currentYear,
-                counter: this.pos.config.sequence || 1,
-            };
-
-            localStorage.setItem(
-                key,
-                JSON.stringify(counterData)
-            );
-        }
-
-        return counterData;
-    },
-
-    _generateReceiptNumber(order) {
-        const config_id = this.pos.config.id;
-
-        const key = this._getCounterKey(config_id);
-
-        let counterData = this._getCounter();
-
-        order.set_pos_order_seq(counterData.counter);
-
-        /*
-            Increment for next order
-        */
-        counterData.counter += 1;
-
-        localStorage.setItem(
-            key,
-            JSON.stringify(counterData)
-        );
-    },
-
     async validateOrder(isForceValidate) {
         const currentOrder = this.currentOrder;
 
-        this._generateReceiptNumber(currentOrder);
+        console.log(this);
         
         // Only process medical equipment if order has extra_data with medical_equipment
         if (currentOrder && currentOrder.extra_data && currentOrder.extra_data.medical_equipment) {
