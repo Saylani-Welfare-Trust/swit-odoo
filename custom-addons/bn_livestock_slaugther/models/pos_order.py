409b5d4a7c9389bdc03cde8bf783179f9a1e1013
Abdulrafay5712 <bytesnode@gmail.com>
2026-08-20 13:15:16 +0500

    Revert "Update pos_order.py"
    This reverts commit b97daae29f73e404c489b8932d980d62ae29397d.

--- custom-addons/bn_livestock_slaugther/models/pos_order.py
+++ custom-addons/bn_livestock_slaugther/models/pos_order.py
@@ -4,20 +4,6 @@ from odoo import api, models
 class POSOrder(models.Model):
     _inherit = 'pos.order'
 
-    def _get_livestock_department_vals(self, product):
-        product_markers = ' '.join(filter(None, [
-            product.display_name,
-            product.default_code,
-            product.product_tmpl_id.name,
-            product.categ_id.complete_name,
-        ])).lower()
-
-        if 'goat' in product_markers:
-            return {'is_goat_depart': True}
-        if 'cow' in product_markers:
-            return {'is_meat_depart': True}
-        return {}
-
     def _create_livestock_slaughter_records(self):
         slaughter_obj = self.env['livestock.slaugther'].sudo()
 
@@ -39,7 +25,7 @@ class POSOrder(models.Model):
                 reference = order.source_document or order.pos_reference or order.name
                 price = line.price_subtotal_incl or line.price_subtotal or line.price_unit * line.qty
 
-                slaughter_vals = {
+                slaughter_obj.create({
                     'product_id': line.product_id.id,
                     'donee_id': order.partner_id.id,
                     'pos_order_id': order.id,
@@ -47,10 +33,7 @@ class POSOrder(models.Model):
                     'quantity': int(line.qty),
                     'price': price,
                     'ref': reference,
-                }
-                slaughter_vals.update(order._get_livestock_department_vals(line.product_id))
-
-                slaughter_obj.create(slaughter_vals)
+                })
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -61,19 +44,4 @@ class POSOrder(models.Model):
     def write(self, vals):
         result = super().write(vals)
         self._create_livestock_slaughter_records()
-        return result
-
-
-class POSOrderLine(models.Model):
-    _inherit = 'pos.order.line'
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        lines = super().create(vals_list)
-        lines.mapped('order_id')._create_livestock_slaughter_records()
-        return lines
-
-    def write(self, vals):
-        result = super().write(vals)
-        self.mapped('order_id')._create_livestock_slaughter_records()
         return result
