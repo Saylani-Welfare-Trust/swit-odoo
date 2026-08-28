d881a8e767fe1b14fd718481c0963432ce18c65e
AbdulHai415 <bytesnode@gmail.com>
2026-08-28 18:51:01 +0500

    On Hand Quantity Medical Equipment

--- custom-addons/bn_medical_equipment/models/medical_equipment_line.py
+++ custom-addons/bn_medical_equipment/models/medical_equipment_line.py
@@ -19,6 +19,17 @@ class DonationHomeServiceLine(models.Model):
         related='medical_equipment_category_id.security_deposit',
         readonly=True
     )
+    # Abdul Hai
+    on_hand_qty = fields.Float(
+        string='On Hand Quantity',
+        compute='_compute_on_hand_qty'
+    )
+    
+    @api.depends('product_id')
+    def _compute_on_hand_qty(self):
+        for rec in self:    
+            rec.on_hand_qty = rec.product_id.qty_available if rec.product_id else 0.0
+        # Abdul Hai
     actual_deposit_percentage = fields.Float(
         string='Actual Deposit Percentage (%)',
         related='medical_equipment_id.actual_deposit_percentage',
