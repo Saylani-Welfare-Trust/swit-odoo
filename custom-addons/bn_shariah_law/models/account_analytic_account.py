028c1d23a9f2927e7c41a9ef0688d82446d30bdf
AbdulHai415 <bytesnode@gmail.com>
2026-08-28 14:44:34 +0500

    .

--- custom-addons/bn_shariah_law/models/account_analytic_account.py
+++ custom-addons/bn_shariah_law/models/account_analytic_account.py
@@ -5,6 +5,6 @@ class AnalyticAccount(models.Model):
     _inherit = 'account.analytic.account'
 
 
-    product_ids = fields.Many2many('product.product', string="Products")
+    # product_ids = fields.Many2many('product.product', string="Products")
 
     member_approval = fields.Boolean(string='Member Approval', default=False, help="If checked, this account requires member approval for transfers.")
