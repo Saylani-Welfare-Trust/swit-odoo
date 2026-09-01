8b00dd02d52f4b849b7eed63e708c32c1abde299
BytesNode Developer <bytesnode@gmail.com>
2026-09-01 17:32:57 +0500

    [1 Sep 26] [Pushing Access Right Module]

---
+++ custom-addons/bn_access_right/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Access Right',
+    'version': '1.0',
+    'author': 'Syed Owais Noor',
+    'website': 'https://bytesnode.com/',
+    'license': 'LGPL-3',
+    'category': 'BytesNode/Access Right',
+    'depends': [],
+    'data': [
+        'data/ir_module_category.xml',
+        'security/groups.xml',
+    ],
+    'auto_install': False,
+    'application': False,
+}
