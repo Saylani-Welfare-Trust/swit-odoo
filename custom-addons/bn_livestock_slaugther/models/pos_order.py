38808dc2d6e0736d7d3e84d885e3fd3040397e2c
AbdulHai415 <bytesnode@gmail.com>
2026-09-01 14:35:50 +0500

    {1 Sep 2026} Working on DN

--- custom-addons/bn_livestock_slaugther/models/pos_order.py
+++ custom-addons/bn_livestock_slaugther/models/pos_order.py
@@ -4,12 +4,12 @@ from odoo import fields as odoo_fields
 
 class POSOrder(models.Model):
     _inherit = 'pos.order'
-    
+
     branch_code = fields.Char(string='Branch Code', help="Short code for the branch")
     counter = fields.Char(string='Counter Number', help="POS session counter number")
     pos_order_seq = fields.Char(string='POS Order Sequence', help="Sequential number from POS")
     dn_number = fields.Char(string='DN Number', copy=False)
-    
+
     @api.model
     def _process_order(self, order_data, draft=False, existing_order=False):
         # Extract custom fields from the frontend data
@@ -22,7 +22,23 @@ class POSOrder(models.Model):
             if data.get('pos_order_seq'):
                 self.pos_order_seq = data['pos_order_seq']
         # Call the original method to create/update the order
-        return super()._process_order(order_data, draft=draft, existing_order=existing_order)
+        order_id = super()._process_order(order_data, draft=draft, existing_order=existing_order)
+        # Ensure DN # is generated/stored once the order exists
+        order = self.browse(order_id) if isinstance(order_id, int) else self.env['pos.order'].browse(order_id)
+        if order and not order.dn_number:
+            order.dn_number = order._compute_dn_number()
+        return order_id
+
+    def _compute_dn_number(self):
+        """Builds the DN # exactly as shown on the receipt header:
+        {branch_code}-C{counter}-{year}-{pos_order_seq}
+        """
+        self.ensure_one()
+        city_code = self.branch_code or 'UNK'
+        counter = self.counter or '0'
+        current_year = fields.Date.context_today(self).year
+        pos_order_seq = self.pos_order_seq or '0000'
+        return f"{city_code}-C{counter}-{current_year}-{pos_order_seq}"
 
     def _get_livestock_department_vals(self, product):
         product_markers = ' '.join(filter(None, [
@@ -37,15 +53,6 @@ class POSOrder(models.Model):
         if 'cow' in product_markers:
             return {'is_meat_depart': True}
         return {}
-    
-    def _get_dn_number(self):
-        self.ensure_one()
-        # These fields are now stored; provide fallbacks just in case
-        city_code = self.branch_code or 'UNK'
-        counter = self.counter or '0'
-        year = self.date_order.year if self.date_order else fields.Date.today().year
-        pos_order_seq = self.pos_order_seq or '0000'
-        return f"{city_code}-C{counter}-{year}-{pos_order_seq}"
 
     def _create_livestock_slaughter_records(self):
         slaughter_obj = self.env['livestock.slaugther'].sudo()
@@ -65,8 +72,10 @@ class POSOrder(models.Model):
                 if existing_record:
                     continue
 
-                # --- NEW: use generated DN number as reference ---
-                reference = order._get_dn_number()
+                # --- Use DN # as reference instead of source_document ---
+                if not order.dn_number:
+                    order.dn_number = order._compute_dn_number()
+                reference = order.dn_number
 
                 price = line.price_subtotal_incl or line.price_subtotal or line.price_unit * line.qty
 
@@ -77,7 +86,7 @@ class POSOrder(models.Model):
                     'pos_order_line_id': line.id,
                     'quantity': int(line.qty),
                     'price': price,
-                    'ref': reference,   # now it's the DN number
+                    'ref': reference,
                 }
                 slaughter_vals.update(order._get_livestock_department_vals(line.product_id))
 
