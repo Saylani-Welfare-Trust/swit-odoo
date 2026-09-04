8b60d57a0e51eb47ee3fdc9f1303e7b1bd207cd5
Aamna Iqbal <bytesnode@gmail.com>
2026-09-04 11:14:51 +0500

    last purchase amount added

--- custom-addons/bn_purchase_customization/models/purchase_order.py
+++ custom-addons/bn_purchase_customization/models/purchase_order.py
@@ -40,7 +40,41 @@ class PurchaseOrderLine(models.Model):
         compute='_compute_on_hand_qty'
     )
 
+    last_purchase_amount = fields.Float(
+        string="Last Purchase Amount",
+        compute='_compute_last_purchase_amount'
+    )
+
+
     @api.depends('product_id')
     def _compute_on_hand_qty(self):
         for line in self:
             line.on_hand_qty = line.product_id.qty_available if line.product_id else 0.0
+
+    @api.depends('product_id')
+    def _compute_last_purchase_amount(self):
+        for line in self:
+            line.last_purchase_amount = 0.0
+
+            if not line.product_id:
+                continue
+
+            domain = [
+                ('state', '=', 'purchase'),
+                ('order_line.product_id', '=', line.product_id.id),
+            ]
+
+            if line.order_id.id:
+                domain.append(('id', '!=', line.order_id.id))
+
+            previous_po = self.env['purchase.order'].search(
+                domain,
+                order='date_approve desc',
+                limit=1
+            )
+
+            if previous_po:
+                previous_line = previous_po.order_line.filtered(
+                    lambda l: l.product_id == line.product_id
+                )[:1]
+                line.last_purchase_amount = previous_line.price_unit
