6a8b5ebee10f98b6d945fe3fc97b5dae1a9ea744
Abdulrafay5712 <bytesnode@gmail.com>
2026-09-04 13:09:51 +0500

    update

--- custom-addons/bn_key_management/models/donation_box_registrtaion_installation.py
+++ custom-addons/bn_key_management/models/donation_box_registrtaion_installation.py
@@ -6,3 +6,16 @@ class DonationBoxRegistrationInstallation(models.Model):
 
 
     key_bunch_id = fields.Many2one('key.bunch', string="Key Bunch", tracking=True)
+
+    def write(self, vals):
+        result = super().write(vals)
+
+        if vals.get('status') == 'close':
+            keys = self.env['key'].search([
+                '|',
+                ('donation_box_registration_installation_id', 'in', self.ids),
+                ('lot_id', 'in', self.mapped('lot_id').ids),
+            ])
+            keys.write({'state': 'closed'})
+
+        return result
