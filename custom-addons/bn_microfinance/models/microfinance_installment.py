99443637e1d9e98c4f4d105e29e9efcdb09f3993
Abdulrafay5712 <bytesnode@gmail.com>
2026-09-07 11:00:00 +0500

    add payment date in microfinance lines

--- custom-addons/bn_microfinance/models/microfinance_installment.py
+++ custom-addons/bn_microfinance/models/microfinance_installment.py
@@ -148,7 +148,7 @@ class MicrofinanceInstallment(models.Model):
                 'paid_amount': data['amount'],
                 'remaining_amount': 0,
                 'state': 'paid',  # This line's state changes to paid
-                'payment_date': fields.Date.today()
+                'payment_date': fields.Date.today(),
             })
             
             return {
