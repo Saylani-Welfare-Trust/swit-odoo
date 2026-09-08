aff1dc4fc5c0b55ec4b7ec059296c529a9b79374
Abdulrafay5712 <bytesnode@gmail.com>
2026-09-08 14:39:37 +0500

    update

--- custom-addons/bn_advance_donation/models/donation_receipt.py
+++ custom-addons/bn_advance_donation/models/donation_receipt.py
@@ -11,7 +11,6 @@ class DonationReceipt(models.Model):
 
     category_id = fields.Many2one('product.category', string='Category')    
     name = fields.Char(string="Name", required=True, copy=False, readonly=True, default=lambda self: _('New'))
-    payment_type = fields.Selection([('cash', 'Cash'), ('cheque', 'Cheque')], string='Payment Method', default='cash')
     is_donation_id = fields.Boolean('Donation ID?')
     donation_id = fields.Many2one('advance.donation', string='Donation ID')
     order_id = fields.Many2one('pos.order', string='POS Order')
@@ -32,6 +31,11 @@ class DonationReceipt(models.Model):
     pos_order_id = fields.Many2one('pos.order', string='POS Order')
     remarks = fields.Text(string='Remarks') 
     product_name = fields.Char(string='Product Name')  # If you want a separate text field
+    payment_type = fields.Selection([
+    ('cash', 'Cash'),
+    ('cheque', 'Cheque'),
+    ('directdeposit', 'Direct Deposit'),
+], string="Payment Type", default='directdeposit')
     state = fields.Selection([
         ('draft', 'Draft'),
         ('pending', 'Pending'),
