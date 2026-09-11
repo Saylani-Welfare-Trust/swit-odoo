1001dfc3abce3552c97e4d0afd7934b283318dc7
Abdulrafay5712 <bytesnode@gmail.com>
2026-09-11 16:42:29 +0500

    add description field

--- custom-addons/bn_material_request/models/material_request_line.py
+++ custom-addons/bn_material_request/models/material_request_line.py
@@ -14,7 +14,7 @@ class MemberApprovalLine(models.Model):
     quantity = fields.Float('Quantity', default=1.0, required=True)
     unit_price = fields.Float('Unit Price', related='product_id.lst_price', store=True)
     subtotal = fields.Float('Subtotal', compute='_compute_subtotal', store=True)
-
+    description = fields.Text('Description')
     allowed_product_category_ids = fields.Many2many(
         related='approval_id.user_id.allowed_product_category_ids',
         readonly=True
