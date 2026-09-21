5f8b0195d3441aace6fcf23556add34c9cf65b2b
BytesNode Developer <bytesnode@gmail.com>
2026-09-21 14:03:38 +0500

    Update audit_rule.py

--- custom-addons/bn_audit_trail/models/audit_rule.py
+++ custom-addons/bn_audit_trail/models/audit_rule.py
@@ -57,15 +57,15 @@ class AuditTrailRule(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         records = super().create(vals_list)
-        self.env.registry.clear_caches()
+        self._get_active_rules.clear_cache(self)
         return records
 
     def write(self, vals):
         res = super().write(vals)
-        self.env.registry.clear_caches()
+        self._get_active_rules.clear_cache(self)
         return res
 
     def unlink(self):
         res = super().unlink()
-        self.env.registry.clear_caches()
+        self._get_active_rules.clear_cache(self)
         return res
