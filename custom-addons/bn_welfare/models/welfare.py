542ff5b435df2afa9091c738885b86b2d973090b
Abdulrafay5712 <bytesnode@gmail.com>
2026-09-21 12:37:36 +0500

    Update welfare.py

--- custom-addons/bn_welfare/models/welfare.py
+++ custom-addons/bn_welfare/models/welfare.py
@@ -1072,6 +1072,17 @@ class Welfare(models.Model):
     
     def _create_donee_in_portal(self):
         """Create donee in Sadqa Jaria portal"""
+        donee = self.donee_id
+
+        # Build a readable single-line address from standard partner fields
+        address_parts = [
+            donee.street,
+            donee.street2,
+            donee.city,
+            donee.state_id.name if donee.state_id else None,
+        ]
+        full_address = ", ".join(part for part in address_parts if part)
+
         data = {
                 "json":{
                 "name": self.donee_id.name or '',
@@ -1079,8 +1090,10 @@ class Welfare(models.Model):
                 "cnic": (
                     self.donee_id.cnic_no.replace("-", "")
                     if self.donee_id.cnic_no else ""
-                ),            
-                "odooId": self.donee_id.id
+                ),
+                "address": full_address,        
+                "odooId": self.donee_id.id,
+                "area" : self.donee_id.area.name if self.donee_id.area else '',
             }
         }
         # raise UserError(str(data))
@@ -1551,8 +1564,8 @@ class Welfare(models.Model):
                 'sticky': False,
             }
         }
-
-
+    
+    
     def action_print_info(self):
         self.ensure_one()
         if not self.donee_id:
