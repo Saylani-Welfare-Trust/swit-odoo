393264438c4d5dd95a519450b8bda77a795d1f55
Abdulrafay5712 <bytesnode@gmail.com>
2026-09-21 13:02:56 +0500

    link the analtical account of product with account

--- custom-addons/bn_inventory_customization/__manifest__.py
+++ custom-addons/bn_inventory_customization/__manifest__.py
@@ -10,6 +10,7 @@
         'product',
         'stock',
         'purchase',
+        'analytic',
         'bn_donation_home_service',
     ],
     'data': [
@@ -19,6 +20,7 @@
         'views/product_attribute_value.xml',
         'views/product_template.xml',
         'views/product_product.xml',
+        'views/account_analytic_account.xml',
         'views/purchase_order.xml',
         'views/stock_picking.xml',
         'wizards/receive_by_weight.xml',
--- custom-addons/bn_inventory_customization/models/__init__.py
+++ custom-addons/bn_inventory_customization/models/__init__.py
@@ -4,3 +4,4 @@ from . import stock_picking
 from . import livestock_variant
 from . import product_attribute_value
 from . import purchase_order
+from . import account_analytic_account
