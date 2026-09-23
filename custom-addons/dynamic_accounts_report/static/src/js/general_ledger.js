d48dbe35e1887156368c2c634a09a3cc0443a754
AbdulHai415 <bytesnode@gmail.com>
2026-09-23 20:52:00 +0500

    Update general_ledger.js

--- custom-addons/dynamic_accounts_report/static/src/js/general_ledger.js
+++ custom-addons/dynamic_accounts_report/static/src/js/general_ledger.js
@@ -2,7 +2,7 @@
 const { Component } = owl;
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
-import { useRef, useState } from "@odoo/owl";
+import { useRef, useState, onMounted } from "@odoo/owl";
 import { BlockUI } from "@web/core/ui/block_ui";
 import { download } from "@web/core/network/download";
 const actionRegistry = registry.category("actions");
