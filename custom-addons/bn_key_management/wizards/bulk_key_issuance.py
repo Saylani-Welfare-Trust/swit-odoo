adc99a79931e454ee1bbb8e7dee900466d458c84
BytesNode Developer <bytesnode@gmail.com>
2026-09-23 16:46:38 +0500

    Update bulk_key_issuance.py

--- custom-addons/bn_key_management/wizards/bulk_key_issuance.py
+++ custom-addons/bn_key_management/wizards/bulk_key_issuance.py
@@ -211,9 +211,7 @@ class BulkKeyIssuance(models.TransientModel):
                 ], order="id desc", limit=1)
 
                 # ❌ If no issuance OR wrong state → block
-                if not issuance:
-                    continue
-                elif issuance or issuance.state not in ['donation_receive', 'pending']:
+                if issuance or issuance.state not in ['donation_receive', 'pending']:
                     invalid_keys.append(key.name)
                 else:
                     valid_issuances.append(issuance)
