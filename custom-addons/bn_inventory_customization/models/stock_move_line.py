d2f39c9d114afec03a2c73df5509bf46371e6d89
AbdulHai415 <bytesnode@gmail.com>
2026-09-25 16:47:23 +0500

    .

--- custom-addons/bn_inventory_customization/models/stock_move_line.py
+++ custom-addons/bn_inventory_customization/models/stock_move_line.py
@@ -1,8 +1,8 @@
 from odoo import models, fields, api
 
 
-class StockMoveLine(models.Model):
-    _inherit = 'stock.move.line'
+class StockMove(models.Model):
+    _inherit = 'stock.move'
 
     on_hand_qty = fields.Float(
         string="On Hand Qty",
@@ -10,19 +10,15 @@ class StockMoveLine(models.Model):
         digits='Product Unit of Measure',
     )
 
-    @api.depends('product_id', 'location_id', 'lot_id')
+    @api.depends('product_id', 'location_id')
     def _compute_on_hand_qty(self):
-        for line in self:
-            if not line.product_id or not line.location_id:
-                line.on_hand_qty = 0.0
+        for move in self:
+            if not move.product_id or not move.location_id:
+                move.on_hand_qty = 0.0
                 continue
 
-            domain = [
-                ('product_id', '=', line.product_id.id),
-                ('location_id', '=', line.location_id.id),
-            ]
-            if line.lot_id:
-                domain.append(('lot_id', '=', line.lot_id.id))
-
-            quants = self.env['stock.quant'].search(domain)
-            line.on_hand_qty = sum(quants.mapped('quantity'))
+            quants = self.env['stock.quant'].search([
+                ('product_id', '=', move.product_id.id),
+                ('location_id', '=', move.location_id.id),
+            ])
+            move.on_hand_qty = sum(quants.mapped('quantity'))
